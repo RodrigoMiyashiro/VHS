4b8b7459f0eef8f77429cabb9791e4572898878d
Rodrigo Miyashiro <rodrigo.miyashiro@homerefill.com.br>
2019-07-03 00:11:36 -0300

    Initial commit.

---
+++ VHS/VHS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VHS
+//
+//  Created by Rodrigo Miyashiro on 03/07/19.
+//  Copyright © 2019 Rodrigo Miyashiro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
